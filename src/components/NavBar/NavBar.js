bc7a0453f3f60060d1a0c7c623ffaf561b84d79f
Lakshya Khera <lakshya.khera@gmail.com>
2020-10-29 14:10:19 +0530

    Added company info dropdown in navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useHistory} from 'react-router-dom'
-import { Navbar,Nav} from 'react-bootstrap'
+import { Navbar,Nav,NavDropdown} from 'react-bootstrap'
 import CacheManager from '../../lib/localStorage'
 const NavBar = () => {
   const history = useHistory()
@@ -20,6 +20,12 @@ const NavBar = () => {
       <Nav.Link onClick={logout}>
         Logout
       </Nav.Link>
+      <NavDropdown title="Company Info" id="collasible-nav-dropdown">
+        <NavDropdown.Item >Company: Geeksynergy Technologies Pvt Ltd</NavDropdown.Item>
+        <NavDropdown.Item >Address: Sanjayanagar, Bengaluru-56</NavDropdown.Item>
+        <NavDropdown.Item >Phone:XXXXXXXXX09</NavDropdown.Item>
+        <NavDropdown.Item >Email: XXXXXX@gmail.com</NavDropdown.Item>
+      </NavDropdown>
     </Nav>
   </Navbar>
   )
